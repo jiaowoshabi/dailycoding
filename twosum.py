490743b73632b8c4766b03cc9de7686e251045e4
Wenhao Zhang <wenhaoz@g.ucla.edu>
2019-06-25 09:01:45 +0800

    add twosum

---
+++ twosum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        string = "Given a list of numbers and a number k, return whether any two numbers from the list add up to k."
+        return string
+
+    def two_sum(self, lst, k):
+        if len(lst) <= 1:
+            return False
+        
+        length = len(lst)
+
+        for idx, val in enumerate(lst):
+            fast = idx + 1
+            while (fast < length) and (lst[idx] + lst[fast] != k):
+                fast += 1
+
+            if (fast < length) and (lst[idx] + lst[fast]) == k:
+                return True
+
+        return False
